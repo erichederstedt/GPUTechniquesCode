660a47a4aa320b97bd5cd881f3855a25ecffc84b
Dolf <eric.jonsson.hederstedt@gmail.com>
2025-06-29 18:46:20 +0200

    Added the nvidia bistro sample Added tangent and bitangent loading from ufbx Begun work on reading in DDS files

--- Code/Rendering/4. PBR  Rendering/main.c
+++ Code/Rendering/4. PBR  Rendering/main.c
@@ -157,6 +157,8 @@ struct Vertex
     Vec3 pos;
     Vec4 color;
     Vec3 normal;
+    Vec3 tangent;
+    Vec3 bitangent;
     Vec2 uv;
 };
 #pragma pack(pop)
@@ -214,6 +216,16 @@ struct Mesh_Part load_mesh_part(ufbx_mesh *mesh, ufbx_mesh_part *part, size_t ma
             {
                 ufbx_vec3 normal = ufbx_get_vertex_vec3(&mesh->vertex_normal, index);
                 conv_float(normal, v->normal);
+            }
+            if (mesh->vertex_tangent.exists)
+            {
+                ufbx_vec3 tangent = ufbx_get_vertex_vec3(&mesh->vertex_tangent, index);
+                conv_float(tangent, v->tangent);
+            }
+            if (mesh->vertex_bitangent.exists)
+            {
+                ufbx_vec3 bitangent = ufbx_get_vertex_vec3(&mesh->vertex_bitangent, index);
+                conv_float(bitangent, v->bitangent);
             }
             if (mesh->vertex_uv.exists)
             {
@@ -324,6 +336,7 @@ struct Node* load_node(ufbx_node* fbx_node, struct Node* root, ufbx_scene* fbx_s
 struct Node* load_fbx(char* path)
 {
     ufbx_load_opts opts = { 0 };
+    // opts.generate_missing_normals = true;
     ufbx_error error;
     ufbx_scene *fbx_scene = ufbx_load_file(path, &opts, &error);
     if (!fbx_scene)
@@ -618,6 +631,18 @@ int CALLBACK WinMain(HINSTANCE CurrentInstance, HINSTANCE PrevInstance, LPSTR Co
             .offset = offsetof(struct Vertex, normal)
         },
         {
+            .element_binding.name = "TANGENT",
+            .format = FORMAT_R32G32B32_FLOAT,
+            .element_classification = INPUT_ELEMENT_CLASSIFICATION_PER_VERTEX,
+            .offset = offsetof(struct Vertex, tangent)
+        },
+        {
+            .element_binding.name = "BITANGENT",
+            .format = FORMAT_R32G32B32_FLOAT,
+            .element_classification = INPUT_ELEMENT_CLASSIFICATION_PER_VERTEX,
+            .offset = offsetof(struct Vertex, bitangent)
+        },
+        {
             .element_binding.name = "UV",
             .format = FORMAT_R32G32_FLOAT,
             .element_classification = INPUT_ELEMENT_CLASSIFICATION_PER_VERTEX,
@@ -686,8 +711,312 @@ int CALLBACK WinMain(HINSTANCE CurrentInstance, HINSTANCE PrevInstance, LPSTR Co
 
         device_create_constant_buffer_view(device, 0, cbv_srv_uav_descriptor_set, camera_constant_buffer, &camera_cbv);
     }
+
+    {
+        char* asset_path = get_asset_path("textures/Awnings_Beams_BaseColor.dds");
+
+        FILE* file = fopen(asset_path, "rb");
+        if (!file)
+            __debugbreak();
+
+        fseek(file, 0L, SEEK_END);
+        size_t file_size = ftell(file);
+        fseek(file, 0L, SEEK_SET);
+
+        char* buffer = malloc(file_size);
+        fread(buffer, 1, file_size, file);
+        fclose(file);
+
+        printf("buffersize: %zd\n", file_size);
+
+        enum DXGI_FORMAT
+        {
+            DXGI_FORMAT_UNKNOWN = 0,
+            DXGI_FORMAT_R32G32B32A32_TYPELESS = 1,
+            DXGI_FORMAT_R32G32B32A32_FLOAT = 2,
+            DXGI_FORMAT_R32G32B32A32_UINT = 3,
+            DXGI_FORMAT_R32G32B32A32_SINT = 4,
+            DXGI_FORMAT_R32G32B32_TYPELESS = 5,
+            DXGI_FORMAT_R32G32B32_FLOAT = 6,
+            DXGI_FORMAT_R32G32B32_UINT = 7,
+            DXGI_FORMAT_R32G32B32_SINT = 8,
+            DXGI_FORMAT_R16G16B16A16_TYPELESS = 9,
+            DXGI_FORMAT_R16G16B16A16_FLOAT = 10,
+            DXGI_FORMAT_R16G16B16A16_UNORM = 11,
+            DXGI_FORMAT_R16G16B16A16_UINT = 12,
+            DXGI_FORMAT_R16G16B16A16_SNORM = 13,
+            DXGI_FORMAT_R16G16B16A16_SINT = 14,
+            DXGI_FORMAT_R32G32_TYPELESS = 15,
+            DXGI_FORMAT_R32G32_FLOAT = 16,
+            DXGI_FORMAT_R32G32_UINT = 17,
+            DXGI_FORMAT_R32G32_SINT = 18,
+            DXGI_FORMAT_R32G8X24_TYPELESS = 19,
+            DXGI_FORMAT_D32_FLOAT_S8X24_UINT = 20,
+            DXGI_FORMAT_R32_FLOAT_X8X24_TYPELESS = 21,
+            DXGI_FORMAT_X32_TYPELESS_G8X24_UINT = 22,
+            DXGI_FORMAT_R10G10B10A2_TYPELESS = 23,
+            DXGI_FORMAT_R10G10B10A2_UNORM = 24,
+            DXGI_FORMAT_R10G10B10A2_UINT = 25,
+            DXGI_FORMAT_R11G11B10_FLOAT = 26,
+            DXGI_FORMAT_R8G8B8A8_TYPELESS = 27,
+            DXGI_FORMAT_R8G8B8A8_UNORM = 28,
+            DXGI_FORMAT_R8G8B8A8_UNORM_SRGB = 29,
+            DXGI_FORMAT_R8G8B8A8_UINT = 30,
+            DXGI_FORMAT_R8G8B8A8_SNORM = 31,
+            DXGI_FORMAT_R8G8B8A8_SINT = 32,
+            DXGI_FORMAT_R16G16_TYPELESS = 33,
+            DXGI_FORMAT_R16G16_FLOAT = 34,
+            DXGI_FORMAT_R16G16_UNORM = 35,
+            DXGI_FORMAT_R16G16_UINT = 36,
+            DXGI_FORMAT_R16G16_SNORM = 37,
+            DXGI_FORMAT_R16G16_SINT = 38,
+            DXGI_FORMAT_R32_TYPELESS = 39,
+            DXGI_FORMAT_D32_FLOAT = 40,
+            DXGI_FORMAT_R32_FLOAT = 41,
+            DXGI_FORMAT_R32_UINT = 42,
+            DXGI_FORMAT_R32_SINT = 43,
+            DXGI_FORMAT_R24G8_TYPELESS = 44,
+            DXGI_FORMAT_D24_UNORM_S8_UINT = 45,
+            DXGI_FORMAT_R24_UNORM_X8_TYPELESS = 46,
+            DXGI_FORMAT_X24_TYPELESS_G8_UINT = 47,
+            DXGI_FORMAT_R8G8_TYPELESS = 48,
+            DXGI_FORMAT_R8G8_UNORM = 49,
+            DXGI_FORMAT_R8G8_UINT = 50,
+            DXGI_FORMAT_R8G8_SNORM = 51,
+            DXGI_FORMAT_R8G8_SINT = 52,
+            DXGI_FORMAT_R16_TYPELESS = 53,
+            DXGI_FORMAT_R16_FLOAT = 54,
+            DXGI_FORMAT_D16_UNORM = 55,
+            DXGI_FORMAT_R16_UNORM = 56,
+            DXGI_FORMAT_R16_UINT = 57,
+            DXGI_FORMAT_R16_SNORM = 58,
+            DXGI_FORMAT_R16_SINT = 59,
+            DXGI_FORMAT_R8_TYPELESS = 60,
+            DXGI_FORMAT_R8_UNORM = 61,
+            DXGI_FORMAT_R8_UINT = 62,
+            DXGI_FORMAT_R8_SNORM = 63,
+            DXGI_FORMAT_R8_SINT = 64,
+            DXGI_FORMAT_A8_UNORM = 65,
+            DXGI_FORMAT_R1_UNORM = 66,
+            DXGI_FORMAT_R9G9B9E5_SHAREDEXP = 67,
+            DXGI_FORMAT_R8G8_B8G8_UNORM = 68,
+            DXGI_FORMAT_G8R8_G8B8_UNORM = 69,
+            DXGI_FORMAT_BC1_TYPELESS = 70,
+            DXGI_FORMAT_BC1_UNORM = 71,
+            DXGI_FORMAT_BC1_UNORM_SRGB = 72,
+            DXGI_FORMAT_BC2_TYPELESS = 73,
+            DXGI_FORMAT_BC2_UNORM = 74,
+            DXGI_FORMAT_BC2_UNORM_SRGB = 75,
+            DXGI_FORMAT_BC3_TYPELESS = 76,
+            DXGI_FORMAT_BC3_UNORM = 77,
+            DXGI_FORMAT_BC3_UNORM_SRGB = 78,
+            DXGI_FORMAT_BC4_TYPELESS = 79,
+            DXGI_FORMAT_BC4_UNORM = 80,
+            DXGI_FORMAT_BC4_SNORM = 81,
+            DXGI_FORMAT_BC5_TYPELESS = 82,
+            DXGI_FORMAT_BC5_UNORM = 83,
+            DXGI_FORMAT_BC5_SNORM = 84,
+            DXGI_FORMAT_B5G6R5_UNORM = 85,
+            DXGI_FORMAT_B5G5R5A1_UNORM = 86,
+            DXGI_FORMAT_B8G8R8A8_UNORM = 87,
+            DXGI_FORMAT_B8G8R8X8_UNORM = 88,
+            DXGI_FORMAT_R10G10B10_XR_BIAS_A2_UNORM = 89,
+            DXGI_FORMAT_B8G8R8A8_TYPELESS = 90,
+            DXGI_FORMAT_B8G8R8A8_UNORM_SRGB = 91,
+            DXGI_FORMAT_B8G8R8X8_TYPELESS = 92,
+            DXGI_FORMAT_B8G8R8X8_UNORM_SRGB = 93,
+            DXGI_FORMAT_BC6H_TYPELESS = 94,
+            DXGI_FORMAT_BC6H_UF16 = 95,
+            DXGI_FORMAT_BC6H_SF16 = 96,
+            DXGI_FORMAT_BC7_TYPELESS = 97,
+            DXGI_FORMAT_BC7_UNORM = 98,
+            DXGI_FORMAT_BC7_UNORM_SRGB = 99,
+            DXGI_FORMAT_AYUV = 100,
+            DXGI_FORMAT_Y410 = 101,
+            DXGI_FORMAT_Y416 = 102,
+            DXGI_FORMAT_NV12 = 103,
+            DXGI_FORMAT_P010 = 104,
+            DXGI_FORMAT_P016 = 105,
+            DXGI_FORMAT_420_OPAQUE = 106,
+            DXGI_FORMAT_YUY2 = 107,
+            DXGI_FORMAT_Y210 = 108,
+            DXGI_FORMAT_Y216 = 109,
+            DXGI_FORMAT_NV11 = 110,
+            DXGI_FORMAT_AI44 = 111,
+            DXGI_FORMAT_IA44 = 112,
+            DXGI_FORMAT_P8 = 113,
+            DXGI_FORMAT_A8P8 = 114,
+            DXGI_FORMAT_B4G4R4A4_UNORM = 115,
+            DXGI_FORMAT_P208 = 130,
+            DXGI_FORMAT_V208 = 131,
+            DXGI_FORMAT_V408 = 132,
+            DXGI_FORMAT_SAMPLER_FEEDBACK_MIN_MIP_OPAQUE = 189,
+            DXGI_FORMAT_SAMPLER_FEEDBACK_MIP_REGION_USED_OPAQUE = 190,
+            DXGI_FORMAT_FORCE_UINT = 0xffffffff
+        };
+        enum D3D10_RESOURCE_DIMENSION
+        {
+            D3D10_RESOURCE_DIMENSION_UNKNOWN = 0,
+            D3D10_RESOURCE_DIMENSION_BUFFER = 1,
+            D3D10_RESOURCE_DIMENSION_TEXTURE1D = 2,
+            D3D10_RESOURCE_DIMENSION_TEXTURE2D = 3,
+            D3D10_RESOURCE_DIMENSION_TEXTURE3D = 4
+        };
+        struct DDS_HEADER_DXT10
+        {
+            enum DXGI_FORMAT                dxgiFormat;
+            enum D3D10_RESOURCE_DIMENSION   resourceDimension;
+            UINT                            miscFlag;
+            UINT                            arraySize;
+            UINT                            miscFlags2;
+        };
+        struct DDS_PIXELFORMAT
+        {
+            DWORD dwSize;
+            DWORD dwFlags;
+            DWORD dwFourCC;
+            DWORD dwRGBBitCount;
+            DWORD dwRBitMask;
+            DWORD dwGBitMask;
+            DWORD dwBBitMask;
+            DWORD dwABitMask;
+        };
+        struct DDS_HEADER
+        {
+            DWORD                   dwSize;
+            DWORD                   dwFlags;
+            DWORD                   dwHeight;
+            DWORD                   dwWidth;
+            DWORD                   dwPitchOrLinearSize;
+            DWORD                   dwDepth;
+            DWORD                   dwMipMapCount;
+            DWORD                   dwReserved1[11];
+            struct DDS_PIXELFORMAT  ddspf;
+            DWORD                   dwCaps;
+            DWORD                   dwCaps2;
+            DWORD                   dwCaps3;
+            DWORD                   dwCaps4;
+            DWORD                   dwReserved2;
+        };
+
+        enum DDS_FLAGS 
+        {
+            DDPF_ALPHAPIXELS =  0x1,
+            DDPF_ALPHA =        0x2,
+            DDPF_FOURCC =       0x4,
+            DDPF_RGB =          0x40,
+            DDPF_YUV =          0x200,
+            DDPF_LUMINANCE =    0x20000,
+        };
+
+        DWORD* dwMagic = (DWORD*)buffer; dwMagic;
+        buffer += sizeof(DWORD);
+        struct DDS_HEADER* header = (struct DDS_HEADER*)buffer;
+        buffer += sizeof(struct DDS_HEADER);
+        struct DDS_HEADER_DXT10* header10 = NULL;
+        char* test = (char*)&header->ddspf.dwFourCC; test;
+        if (header->ddspf.dwFlags & DDPF_FOURCC && !memcmp(&header->ddspf.dwFourCC, "DX10", 4))
+        {
+            header10 = (struct DDS_HEADER_DXT10*)buffer;
+            buffer += sizeof(struct DDS_HEADER_DXT10);
+        }
+
+        if (header10)
+        {
+            /*
+            for (int iArrayElement = 0; iArrayElement < header10->arraySize; iArrayElement++)
+            {
+                for (int iMipLevel = 0; iMipLevel < header->dwMipMapCount; iMipLevel++)
+                {
+                }
+            } 
+            */
+        }
+
+        enum FORMAT texture_format = FORMAT_UNKNOWN;
+        int pre_multiplied_alpha = 0;
+
+        if (header->ddspf.dwFlags & DDPF_FOURCC && !header10)
+        {
+            if (!memcmp(&header->ddspf.dwFourCC, "DXT1", 4))
+                texture_format = FORMAT_BC1_UNORM;
+            if (!memcmp(&header->ddspf.dwFourCC, "DXT3", 4))
+                texture_format = FORMAT_BC2_UNORM;
+            if (!memcmp(&header->ddspf.dwFourCC, "DXT5", 4))
+                texture_format = FORMAT_BC3_UNORM;
+            if (!memcmp(&header->ddspf.dwFourCC, "BC4U", 4))
+                texture_format = FORMAT_BC4_UNORM;
+            if (!memcmp(&header->ddspf.dwFourCC, "BC4S", 4))
+                texture_format = FORMAT_BC4_SNORM;
+            if (!memcmp(&header->ddspf.dwFourCC, "ATI2", 4))
+                texture_format = FORMAT_BC5_UNORM;
+            if (!memcmp(&header->ddspf.dwFourCC, "BC5S", 4))
+                texture_format = FORMAT_BC5_SNORM;
+            if (!memcmp(&header->ddspf.dwFourCC, "RGBG", 4))
+                texture_format = FORMAT_R8G8_B8G8_UNORM;
+            if (!memcmp(&header->ddspf.dwFourCC, "GRGB", 4))
+                texture_format = FORMAT_G8R8_G8B8_UNORM;
+            if (header->ddspf.dwFourCC == 36)
+                texture_format = FORMAT_R16G16B16A16_UNORM;
+            if (header->ddspf.dwFourCC == 110)
+                texture_format = FORMAT_R16G16B16A16_SNORM;
+            if (header->ddspf.dwFourCC == 111)
+                texture_format = FORMAT_R16_FLOAT;
+            if (header->ddspf.dwFourCC == 112)
+                texture_format = FORMAT_R16G16_FLOAT;
+            if (header->ddspf.dwFourCC == 113)
+                texture_format = FORMAT_R16G16B16A16_FLOAT;
+            if (header->ddspf.dwFourCC == 114)
+                texture_format = FORMAT_R32_FLOAT;
+            if (header->ddspf.dwFourCC == 115)
+                texture_format = FORMAT_R32G32_FLOAT;
+            if (header->ddspf.dwFourCC == 116)
+                texture_format = FORMAT_R32G32B32A32_FLOAT;
+            if (!memcmp(&header->ddspf.dwFourCC, "DXT2", 4))
+            {
+                texture_format = FORMAT_BC1_UNORM;
+                pre_multiplied_alpha = 1;
+            }
+            if (!memcmp(&header->ddspf.dwFourCC, "DXT4", 4))
+            {
+                texture_format = FORMAT_BC2_UNORM;
+                pre_multiplied_alpha = 1;
+            }
+            if (!memcmp(&header->ddspf.dwFourCC, "UYVY", 4))
+            {
+                texture_format = FORMAT_R8G8_B8G8_UNORM;
+                printf("Unsupported format!\n");
+            }
+            if (!memcmp(&header->ddspf.dwFourCC, "YUY2", 4))
+            {
+                texture_format = FORMAT_G8R8_G8B8_UNORM;
+                printf("Unsupported format!\n");
+            }
+            if (header->ddspf.dwFourCC == 117)
+            {
+                texture_format = FORMAT_R8G8_SNORM;
+                printf("Unsupported format!\n");
+            }
+        }
+        else if (!header10)
+        {
+            // Handle the pre-ddspf cluster fuck
+        }
+        else
+        {
+            // Handle format shit from header10
+        }
+        
+        for (unsigned int iMipLevel = 0; iMipLevel < header->dwMipMapCount; iMipLevel++)
+        {
+
+        }
+
+        __debugbreak();
+        if (1) return 0;
+    }
     
-    char* asset_path = get_asset_path("NewSponza_Main_Yup_003_bin.fbx");
+    char* asset_path = get_asset_path("Sponza.fbx");
     struct Node* scene_node = load_fbx(asset_path);
     scene_node->local_scale = V3(0.01f, 0.01f, 0.01f);
     free(asset_path);
--- Code/Rendering/4. PBR  Rendering/shader.hlsl
+++ Code/Rendering/4. PBR  Rendering/shader.hlsl
@@ -3,6 +3,8 @@ struct vs_in
     float3 pos : POS;
     float4 color : COL;
     float3 normal : NORMAL;
+    float3 tangent : TANGENT;
+    float3 bitangent : BITANGENT;
     float2 uv : UV;
 };
 
@@ -10,6 +12,9 @@ struct vs_out
 {
     float4 ws_pos : WS_POSITION;
     float4 cs_pos : SV_POSITION;
+    float4 ws_normal : WS_NORMAL;
+    float4 ws_tangent : WS_TANGENT;
+    float4 ws_bitangent : WS_BITANGENT;
     float4 color : COL;
     float2 uv : UV;
 };
@@ -30,9 +35,12 @@ Texture2D color_texture : register(t0);
 vs_out VSMain(vs_in In)
 {
     vs_out Out;
-    
+
     Out.ws_pos = mul(model_to_world, float4(In.pos, 1.0));
     Out.cs_pos = mul(world_to_clip, Out.ws_pos);
+    Out.ws_normal = normalize(mul(model_to_world, float4(In.normal, 0.0)));
+    Out.ws_tangent = normalize(mul(model_to_world, float4(In.tangent, 0.0)));
+    Out.ws_bitangent = normalize(mul(model_to_world, float4(In.bitangent, 0.0)));
     Out.color = In.color;
     In.uv.y = In.uv.y;
     Out.uv = In.uv;
@@ -50,8 +58,10 @@ float4 PSMain(vs_out In) : SV_TARGET
 {
     float4 color = color_texture.Sample(Sampler, In.uv);
 
-    if (color.a < 0.5f)
-        discard;
+    // if (color.a < 0.5f)
+        // discard;
 
+    color.rgb = (In.ws_normal.rgb);
+    color.a = 1.0;
     return color;
 }
